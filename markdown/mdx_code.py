10509cd461c7b32043c425afaeba3d114697a9ac
Will Larson <lethain@gmail.com>
2008-06-23 23:06:47 +0900

    Cleaned up code.

--- markdown/mdx_code.py
+++ markdown/mdx_code.py
@@ -17,8 +17,6 @@ class CodeExtension (markdown.Extension):
 
 
 CODE_BLOCK_REGEX = re.compile(r"@@ (?P<syntax>\w+)\r?\n(?P<code>.*?)@@\r?\n", re.DOTALL)
-CODE_BLOCK_END = re.compile(r"@@\r?\n")
-
 
 class CodeBlockPreprocessor :
     def run (self, text):
@@ -27,8 +25,7 @@ class CodeBlockPreprocessor :
             if not m: break;
             lexer = get_lexer_by_name(m.group('syntax'))
             color = highlight(m.group('code'), lexer, HtmlFormatter())
-            code = u"<pre><code>%s</code></pre>" % color
-            placeholder = self.md.htmlStash.store(code, safe=True)
+            placeholder = self.md.htmlStash.store(color, safe=True)
             text = '%s\n%s\n%s'% (text[:m.start()], placeholder, text[m.end():])
         return text
 
