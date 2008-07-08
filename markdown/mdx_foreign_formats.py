e66fa35af28b844d0ad5d35472b2e4807f4651ec
Will Larson <lethain@gmail.com>
2008-07-08 17:38:34 +0900

    Removed dependency on Django from mdx_foreign_formats, such that it could now be used by non-Django apps.

--- markdown/mdx_foreign_formats.py
+++ markdown/mdx_foreign_formats.py
@@ -1,6 +1,22 @@
 import re
 from lifeflow.markdown import markdown
-from django.utils.encoding import smart_str, force_unicode
+
+def smart_str(s, encoding='utf-8', errors='strict'):
+    """
+    Returns a bytestring version of 's', encoded as specified in 'encoding'.
+    Borrowed and simplified for this purpose from `django.utils.encoding`.
+    """
+    if not isinstance(s, basestring):
+        try:
+            return str(s)
+        except UnicodeEncodeError:
+            return unicode(s).encode(encoding, errors)
+    elif isinstance(s, unicode):
+        return s.encode(encoding, errors)
+    elif s and encoding != 'utf-8':
+        return s.decode('utf-8', errors).encode(encoding, errors)
+    else:
+        return s
 
 class ForeignFormatsExtension (markdown.Extension):
 
